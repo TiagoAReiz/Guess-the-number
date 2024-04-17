d2a9895c350418a960d48aa223785415ea56e302
Tiago <tiagoreizzzz@gmail.com>
2024-04-17 13:43:30 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let randomNumber = Math.floor(Math.random() * 100)
+let tries = 0
+function checkGuess() {
+  let guess = document.getElementById("guess").value
+  let message = document.getElementById("message")
+  tries++
+  if (guess == randomNumber) {
+    message.innerHTML =
+      "Congrats! You guessed correctly in " + tries + " attempts"
+  } else if (guess < randomNumber) {
+    message.innerHTML = "Your guess is too low"
+  } else {
+    message.innerHTML = "Your guess is too high"
+  }
+}
